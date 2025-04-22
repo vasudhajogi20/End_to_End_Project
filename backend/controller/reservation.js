c47e4d0b8ef3886c2450210ad09e655a930cbc9c
vasudhajogi20 <vasudhajogi@gmail.com>
2025-04-22 13:19:40 +0530

    Initial commit - frontend & backend added

---
+++ backend/controller/reservation.js
@@ -0,0 +1,34 @@
+import ErrorHandler from "../error/error.js";
+import { Reservation } from "../models/reservationSchema.js";
+
+export const sendReservation = async(req, res, next)=>{
+    const {firstName, lastName, email, phone, date, time} = req.body;
+    if(!firstName || !lastName || !email || !phone || !date || !time){
+        return next(new ErrorHandler("Please fill full reservation form!",400));
+    }
+    try{
+        await Reservation.create({
+            firstName,
+            lastName,
+            email,
+            phone,
+            date,
+            time
+        });
+        
+        res.status(200).json({
+            success: true,
+            message: "Reservation sent successfully",
+        })
+    }catch (error) {
+        if(error.name === "validationError"){
+            const validationErrors = Object.values(error.errors).map(
+                (err) => err.message
+            );
+            return next(new ErrorHandler(validationErrors.join(" , "), 400));
+        }
+        return next(error)
+
+    }
+
+};
