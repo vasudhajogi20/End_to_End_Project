1b93226bac5dbaa11db3863e0b8e2a4be3ac7524
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-04-27 17:58:26 +0530

    Update About.jsx

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,16 +10,10 @@ const About = () => {
           <div className="banner">
             <div className="top">
               <h1 className="heading">ABOUT US</h1>
-              <p>The only thing we're serious about is food.</p>
+              <p>Our passion for authentic South Indian cuisine is unmatched.</p>
             </div>
             <p className="mid">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident possimus optio adipisci dolores impedit illum iusto
-              perferendis, laudantium quod accusamus consequuntur consectetur,
-              tempore nulla error iure reiciendis dolorem assumenda.
-              Necessitatibus fugit asperiores totam rem esse exercitationem
-              iusto ipsum qui dolore ex, accusantium repellat mollitia
-              repellendus.
+              Welcome to our South Indian restaurant, where tradition and taste come together to create a delightful culinary experience. We are committed to serving you the finest and freshest dishes, from crispy dosas to rich sambar, all made with the highest quality ingredients. Whether you're a fan of the spicy flavors of Chettinad cuisine or the creamy goodness of coconut-based gravies, our menu offers a variety of dishes that reflect the diverse flavors of South India. Come indulge in a meal that will transport your taste buds to the heart of the South!
             </p>
             <Link to={"/"}>
               Explore Menu{" "}
