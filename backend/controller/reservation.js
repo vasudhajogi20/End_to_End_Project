a8e4b467c7d9820873c80cd4f12ea270096752c8
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-05-21 17:36:17 +0530

    Update reservation.js

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,34 +1,32 @@
 import ErrorHandler from "../error/error.js";
 import { Reservation } from "../models/reservationSchema.js";
+import { emitTableAvailabilityUpdate } from "../server.js";
+import { getUpdatedAvailability } from "../utils/availability.js";
 
-export const sendReservation = async(req, res, next)=>{
-    const {firstName, lastName, email, phone, date, time} = req.body;
-    if(!firstName || !lastName || !email || !phone || !date || !time){
-        return next(new ErrorHandler("Please fill full reservation form!",400));
+export const sendReservation = async (req, res, next) => {
+    const { firstName, lastName, email, phone, date, time } = req.body;
+
+    if (!firstName || !lastName || !email || !phone || !date || !time) {
+        return next(new ErrorHandler("Please fill full reservation form!", 400));
     }
-    try{
-        await Reservation.create({
-            firstName,
-            lastName,
-            email,
-            phone,
-            date,
-            time
-        });
+
+    try {
+        const reservation = await Reservation.create({ firstName, lastName, email, phone, date, time });
+
         
+        const updatedAvailability = await getUpdatedAvailability(date, time);
+        emitTableAvailabilityUpdate(updatedAvailability);
+
         res.status(200).json({
             success: true,
             message: "Reservation sent successfully",
-        })
-    }catch (error) {
-        if(error.name === "ValidationError"){
-            const validationErrors = Object.values(error.errors).map(
-                (err) => err.message
-            );
-            return next(new ErrorHandler(validationErrors.join(" , "), 400));
+            reservation,
+        });
+    } catch (error) {
+        if (error.name === "ValidationError") {
+            const validationErrors = Object.values(error.errors).map(err => err.message);
+            return next(new ErrorHandler(validationErrors.join(", "), 400));
         }
-        return next(error)
-
+        return next(error);
     }
-
 };
