28c7eca847edba54593d1db70700cfff45d3aa05
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-05-21 17:37:36 +0530

    Update error.js

--- backend/error/error.js
+++ backend/error/error.js
@@ -1,19 +1,21 @@
-class ErrorHandler extends Error{
-    constructor(message, statusCode){
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
-
     }
 }
 
-export const errorMiddleware = (err, req, res, next)=>{
+export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal server error!";
     err.statusCode = err.statusCode || 500;
+
+    if (process.env.NODE_ENV !== "production") {
+        console.error(err.stack);
+    }
 
     return res.status(err.statusCode).json({
         success: false,
         message: err.message,
-
     });
 };
 
