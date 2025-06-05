a8cc4fe9a712975b68edce8c0f20454bf42193f1
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-06-05 11:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,16 @@
-import http from "http";
-import dotenv from "dotenv";
-import app from "./app.js";
-import { Server } from "socket.io";
 
-dotenv.config();
+import app from "./app.js";
+import http from "http";
+import { initSocket } from "./socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: process.env.FRONTEND_URL,
-        methods: ["GET", "POST"],
-    },
-});
 
-io.on("connection", (socket) => {
-    console.log(`Client connected: ${socket.id}`);
-
-    socket.on("disconnect", () => {
-        console.log(`Client disconnected: ${socket.id}`);
-    });
-});
-
-export function emitTableAvailabilityUpdate(data) {
-    io.emit("tableAvailabilityUpdate", data);
-}
+initSocket(server);
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
