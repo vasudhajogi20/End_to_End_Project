9b43f10d81d69817498b32d97d8062ce84cdbaf9
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-06-05 11:10:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,25 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./error/error.js";
-import reservationRouter from "./routes/reservationRoutes.js";
+import reservationRouter from './routes/reservationRoute.js';
+
 
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST", "GET"],
-    credentials: true,
+  origin: [process.env.FRONTEND_URL],
+  methods: ["POST", "GET"],
+  credentials: true,
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
-
-app.use("/api/v1/reservation", reservationRouter);
+app.use('/api/v1/reservation', reservationRouter);
 
 dbConnection();
 
