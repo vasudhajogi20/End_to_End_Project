5a28f3a3d7ccb35952896c6ecac4f0a88099dd26
vasudhajogi20 <141392511+vasudhajogi20@users.noreply.github.com>
2025-06-05 11:07:51 +0530

    Update reservation.js

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -1,32 +1,40 @@
 import ErrorHandler from "../error/error.js";
-import { Reservation } from "../models/reservationSchema.js";
-import { emitTableAvailabilityUpdate } from "../server.js";
+import ReservationModel from "../models/ReservationModel.js"; 
+import { emitTableAvailabilityUpdate } from "../socket.js";
 import { getUpdatedAvailability } from "../utils/availability.js";
 
 export const sendReservation = async (req, res, next) => {
-    const { firstName, lastName, email, phone, date, time } = req.body;
+  const { firstName, lastName, email, phone, date, time } = req.body;
 
-    if (!firstName || !lastName || !email || !phone || !date || !time) {
-        return next(new ErrorHandler("Please fill full reservation form!", 400));
+  if (!firstName || !lastName || !email || !phone || !date || !time) {
+    return next(new ErrorHandler("Please fill full reservation form!", 400));
+  }
+
+  try {
+    const reservation = await ReservationModel.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+      date,
+      time,
+    });
+
+    const updatedAvailability = await getUpdatedAvailability(date, time);
+    emitTableAvailabilityUpdate(updatedAvailability);
+
+    res.status(200).json({
+      success: true,
+      message: "Reservation sent successfully",
+      reservation,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      const validationErrors = Object.values(error.errors).map(
+        (err) => err.message
+      );
+      return next(new ErrorHandler(validationErrors.join(", "), 400));
     }
-
-    try {
-        const reservation = await Reservation.create({ firstName, lastName, email, phone, date, time });
-
-        
-        const updatedAvailability = await getUpdatedAvailability(date, time);
-        emitTableAvailabilityUpdate(updatedAvailability);
-
-        res.status(200).json({
-            success: true,
-            message: "Reservation sent successfully",
-            reservation,
-        });
-    } catch (error) {
-        if (error.name === "ValidationError") {
-            const validationErrors = Object.values(error.errors).map(err => err.message);
-            return next(new ErrorHandler(validationErrors.join(", "), 400));
-        }
-        return next(error);
-    }
+    return next(error);
+  }
 };
